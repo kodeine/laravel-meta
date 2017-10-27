40578be6455ced0233398e0711f68f3abf801c88
Alexandre Rimthong <rimthong@gmail.com>
2017-10-27 14:35:54 -0400

    Fixes eager loading

--- src/Kodeine/Metable/Metable.php
+++ src/Kodeine/Metable/Metable.php
@@ -4,6 +4,7 @@ namespace Kodeine\Metable;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Collection as BaseCollection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait Metable
 {
