2826b9d4b6f9d4d12ea97615a660280c519625c8
Siamak Salimi <siamak.salimi.2@gmail.com>
2023-07-01 00:03:18 +0330

    Fixed some errors in UserCastedObject.php

--- tests/Casts/UserCastedObject.php
+++ tests/Casts/UserCastedObject.php
@@ -7,20 +7,18 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class UserCastedObject implements Castable, CastsAttributes
 {
-	public string $description;
-
-    public function get($model, $key, $value, $attributes)
-    {
-        return json_decode($value, true);
-    }
-
-    public function set($model, $key, $value, $attributes)
-    {
-        return [$key => json_encode($value)];
-    }
-
-	public static function castUsing(array $arguments)
-	{
-		return new self(...$arguments);
+	public $description;
+	
+	public function get($model, $key, $value, $attributes) {
+		if ( is_null( $value ) ) return null;
+		return json_decode( $value, true );
+	}
+	
+	public function set($model, $key, $value, $attributes) {
+		return [$key => json_encode( $value )];
+	}
+	
+	public static function castUsing(array $arguments) {
+		return new self();
 	}
 }
